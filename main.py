74f65ef53ba6846367316dcd9c92658ee9c55d69
yannraa <lm_remadna@esi.dz>
2023-12-24 17:56:36 +0100

    bdd

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import modelsmysql
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from enum import Enum
-from modelsmysql import Utilisateur
+from modelsmysql import Utilisateur, Moderateur, Administrateur
 app = FastAPI()
 modelsmysql.Base.metadata.create_all(bind=engine)
 class PostBase(BaseModel):
@@ -22,6 +22,18 @@ class UtilisateurBase(BaseModel):
     motdepasse : str
     sexe : Sexe
     email : EmailStr
+class AdministrateurBase(BaseModel):
+    nom : str
+    motdepasse : str
+    sexe : Sexe
+    email : EmailStr
+    
+class ModerateurBase(BaseModel):
+     nom : str
+     motdepasse : str
+     sexe : Sexe
+     email : EmailStr
+    
 
 def get_db():
     db = SessionLocal()
@@ -69,3 +81,36 @@ async def read_utilisateur(utilisateur_id: int, db: db_dependency):
         raise HTTPException(status_code=404,detail='Utilisateur non trouvé')
     
     return utilisateur
+
+
+@app.post("/moderateurs/", status_code=status.HTTP_201_CREATED)
+async def create_moderateur(moderateur: ModerateurBase, db: db_dependency):
+    db_moderateur = modelsmysql.Moderateur(**moderateur.dict())
+    db.add(db_moderateur)
+    db.commit()
+    db.refresh(db_moderateur)
+
+
+@app.get("/moderateurs/{moderateur_id}",status_code=status.HTTP_200_OK)
+async def read_moderateur(moderateur_id: int, db: db_dependency):
+    moderateur = db.query(modelsmysql.moderateur).filter(modelsmysql.moderateur.id == moderateur_id).first()
+    if  moderateur is None:
+        raise HTTPException(status_code=404,detail='Moderateur non trouvé')
+    
+    return moderateur
+
+@app.post("/administrateurs/", status_code=status.HTTP_201_CREATED)
+async def create_administrateur(administrateur: AdministrateurBase, db: db_dependency):
+    db_administrateur = modelsmysql.Administrateur(**administrateur.dict())
+    db.add(db_administrateur)
+    db.commit()
+    db.refresh(db_administrateur)
+    return db_administrateur
+
+
+@app.get("/administrateurs/{administrateur_id}", status_code=status.HTTP_200_OK)
+async def read_administrateur(administrateur_id: int, db: db_dependency):
+    administrateur = db.query(modelsmysql.Administrateur).filter(modelsmysql.Administrateur.id == administrateur_id).first()
+    if administrateur is None:
+        raise HTTPException(status_code=404, detail='Administrateur non trouvé')
+    return administrateur
--- modelsmysql.py
+++ modelsmysql.py
@@ -18,10 +18,50 @@ class Utilisateur(Base):
             name="utilisateurs_sexe_check",
         ),
     )
+    
+class Moderateur(Base):
+         __tablename__='moderateurs'
+         id = Column(Integer, primary_key=True, index=True)
+         nom = Column(String(50))
+         motdepasse= Column(String(50),unique=True,nullable =False)
+         sexe = Column(String(10), nullable=False)
+         email = Column(String(100), unique=True, nullable=False)
 
+         __table_args__ = (
+             CheckConstraint(
+             "email REGEXP '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$'",
+             name="moderateurs_email_format_check",
+             ),
+             CheckConstraint(
+             "sexe IN ('masculin', 'féminin')",
+             name="moderateurs_sexe_check",
+             ),
+         )
+
+ 
 class Post(Base):
         __tablename__='posts'
         id = Column(Integer, primary_key=True, index=True)
         titre = Column(String(50))
         contenu = Column(String(100))
         utilisateur_id = Column(Integer)
+
+
+class Administrateur(Base):
+    __tablename__ = 'administrateurs'
+    id = Column(Integer, primary_key=True, index=True)
+    nom = Column(String(50))
+    motdepasse = Column(String(50), unique=True, nullable=False)
+    sexe = Column(String(10), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+
+    __table_args__ = (
+        CheckConstraint(
+            "email REGEXP '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$'",
+            name="administrateurs_email_format_check",
+        ),
+        CheckConstraint(
+            "sexe IN ('masculin', 'féminin')",
+            name="administrateurs_sexe_check",
+        ),
+    )
